85cdac6cce36e5b8359c5e7fdc17a535f33b0f81
yangxcc <1434890515@qq.com>
2020-04-10 22:14:00 +0800

    对Users类中的方法进行单元测试

--- test/sevice/impl/TestUserDaoImpl.java
+++ test/sevice/impl/TestUserDaoImpl.java
@@ -7,13 +7,38 @@ import org.junit.Test;
 import service.UsersDao;
 import service.impl.UserDaoImpl;
 
+import java.util.List;
+
 public class TestUserDaoImpl {
 
-    @Test
+
     public void testusersLogin(){
         Users users = new Users(1,"zhangsan","123456");
         UsersDao uDao = new UserDaoImpl();
         //Assert.assertEquals(true,uDao.usersLogin(users));
         Assert.assertEquals(true,uDao.usersLogin(users));
+    }
+
+    public void testQueryAllUsers(){
+        UsersDao udao = new UserDaoImpl();
+        List<Users> list = udao.queryAllUsers();
+        for (int i=0;i<list.size();i++){
+            System.out.println(list.get(i));
+        }
+    }
+
+
+    public void testGetNewUid(){
+        UserDaoImpl uDao = new UserDaoImpl();
+        System.out.println(uDao.getNewUid());
+    }
+
+    @Test
+    public void testAddUsers(){
+        Users user = new Users();
+        user.setUsername("lisi");
+        user.setPassword("123456");
+        UsersDao uDao = new UserDaoImpl();
+        Assert.assertEquals(true,uDao.addUsers(user));
     }
 }
